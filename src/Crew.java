11e8db11e40cc8ef917fc8747bf7b82824c8b985
MateuszMarter <mateuszmarter@gmail.com>
2025-03-17 18:56:08 +0100

    crew

--- src/Crew.java
+++ src/Crew.java
@@ -3,7 +3,26 @@ import java.util.List;
 
 public class Crew {
     private String id;
-    private static final List workers = new ArrayList<CrewWorker>();
+    private List workers;
 
-    
+    public Crew(String id, List<CrewWorker> workers) {
+        setId(id);
+        setWorkers(workers);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setWorkers(List<CrewWorker> workers) {
+        this.workers = workers;
+    }
+
+    public void addWorker(CrewWorker worker) {
+        if(worker != null) {
+            this.workers.add(worker);
+        } else {
+            throw new NullPointerException();
+        }
+    }
 }
